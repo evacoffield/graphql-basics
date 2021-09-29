5731e39d2bfe313af11e04e62fad4be6f1b00681
Eva Coffield <eva@darcoff.com>
2021-09-29 22:01:56 +0100

    add updatePost mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -84,6 +84,25 @@ const Mutation = {
       db.comments = db.comments.filter((comment) => comment.post !== args.id)
       return deletedPosts[0]
   },
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args
+    const post = db.posts.find((post) => post.id === id)
+    if (!post) {
+        throw new Error('Post not found')
+    }
+    if (typeof data.title === 'string') {
+        post.title = data.title
+    }
+
+    if (typeof data.body === 'string') {
+        post.body = data.body
+    }
+
+    if (typeof data.published === 'boolean') {
+        post.published = data.published
+    }
+    return post
+},
   createComment(parent, args, { db }, info) {
       //Make sure that a users exists
       const userExists = db.users.some((user) => user.id === args.data.author)
